d3ec7072ebb6a5a1a1d1f61f94c7129b71864a5c
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-12-19 16:29:33 +0100

    first commit

---
+++ src/test/java/ch/bbw/ms/TestApplication/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.ms.TestApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
